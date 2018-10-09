d73f331d9c0eec4d57c2cfdfbaad7f094414fce4
jesuswr <jesuswahrman@gmail.com>
2018-10-09 16:02:40 -0400

    Parte del codigo de prueba de tiempo

--- TestTime.py
+++ TestTime.py
@@ -1,4 +1,4 @@
-import time
+'''import time
 
 n = 1000
 j=0
@@ -10,4 +10,31 @@ fin = time.time()
 
 tiempoTranscurrido = (fin-inicio) * 1000 # para expresarlo en milisegundos
 
-print("Tiempo: " + str(tiempoTranscurrido))
+print("Tiempo: " + str(tiempoTranscurrido))'''
+
+import time
+import sys
+from Sorts import MergeSort
+
+MetodoDeOrdenamiento = str(sys.argv[1])
+NumeroDeElementos = int(sys.argv[2])
+TiempoInicial = 0
+TiempoFinal = 0
+TiempoParaOrdenar = 0
+
+Lista=[3,1,4,5,1,3,4,2,0,13,4,1,3,6,8,2,5,25,14,1467,515,141]#Esta lista es para probar
+
+if MetodoDeOrdenamiento == "MergeSort":
+	
+	TiempoInicial = time.time()
+	
+	ListaOrdenada = MergeSort (Lista, 0, NumeroDeElementos)
+
+	TiempoFinal = time.time()
+
+#Aqui iria el elif
+
+
+TiempoParaOrdenar = (TiempoFinal - TiempoInicial)*1000
+
+print(MetodoDeOrdenamiento, NumeroDeElementos, TiempoParaOrdenar)
